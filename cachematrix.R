0dd6bb8cf4ef1465f8174fed1d0fe5bde41655c1
do-jo <ic3manx@gmail.com>
2015-05-23 12:32:37 -0700

    r_programming_assignment2
    solve for and cache the inverse of a matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+###########################################################
+##These functions solve for and cache the inverse of a matrix, allowing efficient access.
+#########################################################
 
-## Write a short comment describing this function
+
+## The maKeCacheMatrix function creates a special matrix that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  #reset the inverse to null
+  inverse = NULL 
+  
+  #define variable x in new environment; make inverse NULL since a new matrix is defined
+  set <- function(y) {
+    x <<- y  
+    inverse <<- NULL 
+  }
+  
+  #retrieve the matrix
+  get <- function() {
+    x
+  }
+  
+  #set the value of the inverse
+  setInverse <- function(inv) {
+    inverse <<- inv
+  }
+  
+  #retrieve the value of the inverse
+  getInverse <- function() {
+    inverse
+  }
 
+  #return these variables
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## The cacheSolve function computes the inverse of the matrix returned by the makeCacheMatrix function, or retrieves it from the cache if it exists. ***The function assumes that the matrix is invertible
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #retrieve cached matrix inverse if it exists
+  inverse = x$getInverse()
+  if(!is.null(inverse)) { 
+    message('Retrieving cached data')
+    return(inverse)
+  } else {
+  #calculate and set the cached inverse it if does NOT yet exist
+    data = x$get()
+    inverse = solve(data)
+    x$setInverse(inverse)
+    inverse
+  }
 }
+
+
+## Example Usage:
+
+# testMatrix = matrix(c(3,4,2,3), nc = 2, byrow = TRUE) #define a matrix
+# a = makeCacheMatrix(testMatrix) #cache the matrix
+# a$get() #retrieve the matrix
+# cacheSolve(a) #solve for the matrix inverse
+# a$getInverse() #retrieve the matrix inverse
+
+
